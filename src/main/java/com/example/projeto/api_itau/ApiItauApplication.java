90ad94db66cdc09f273abd8d4208f4e47f394ba2
Marcos <marcosviniramos62@gmail.com>
2025-09-27 16:12:20 -0300

    feat: Estrutura inicial do projeto gerada pelo Spring Initializr

---
+++ src/main/java/com/example/projeto/api_itau/ApiItauApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto.api_itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projeto/api_itau/ApiItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto.api_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
